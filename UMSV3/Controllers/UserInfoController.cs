df49894b71e43259efa4f9f2f11cedf523b1da8a
Kevin Lopez <kevin.lopez@email.com>
2020-09-08 00:17:41 -0400

    Add project files.

---
+++ UMSV3/Controllers/UserInfoController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using UMSV3.Models;
+
+namespace UMSV3.Controllers
+{
+    public class UserInfoController : Controller
+    {
+        private UMSEntities db = new UMSEntities();
+
+        public ActionResult Login() {
+            return View();
+        }
+        // GET: UserInfo
+        public ActionResult Index()
+        {
+            var userInfoes = db.UserInfoes.Include(u => u.Role).Include(u => u.Status).Include(u => u.UserCredential);
+            return View(userInfoes.ToList());
+        }
+
+        // GET: UserInfo/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserInfo userInfo = db.UserInfoes.Find(id);
+            if (userInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userInfo);
+        }
+
+        // GET: UserInfo/Create
+        public ActionResult Create()
+        {
+            ViewBag.RoleId = new SelectList(db.Roles, "RoleId", "RoleName");
+            ViewBag.StatusId = new SelectList(db.Status, "StatusId", "StatusName");
+            ViewBag.UserId = new SelectList(db.UserCredentials, "UserId", "Password");
+            return View();
+        }
+
+        // POST: UserInfo/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "UserId,UserName,FirstName,LastName,Email,C_Address,City,Country,ZipCode,PhoneNumber,StatusId,RoleId")] UserInfo userInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UserInfoes.Add(userInfo);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.RoleId = new SelectList(db.Roles, "RoleId", "RoleName", userInfo.RoleId);
+            ViewBag.StatusId = new SelectList(db.Status, "StatusId", "StatusName", userInfo.StatusId);
+            ViewBag.UserId = new SelectList(db.UserCredentials, "UserId", "Password", userInfo.UserId);
+            return View(userInfo);
+        }
+
+        // GET: UserInfo/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserInfo userInfo = db.UserInfoes.Find(id);
+            if (userInfo == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.RoleId = new SelectList(db.Roles, "RoleId", "RoleName", userInfo.RoleId);
+            ViewBag.StatusId = new SelectList(db.Status, "StatusId", "StatusName", userInfo.StatusId);
+            ViewBag.UserId = new SelectList(db.UserCredentials, "UserId", "Password", userInfo.UserId);
+            return View(userInfo);
+        }
+
+        // POST: UserInfo/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "UserId,UserName,FirstName,LastName,Email,C_Address,City,Country,ZipCode,PhoneNumber,StatusId,RoleId")] UserInfo userInfo)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(userInfo).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.RoleId = new SelectList(db.Roles, "RoleId", "RoleName", userInfo.RoleId);
+            ViewBag.StatusId = new SelectList(db.Status, "StatusId", "StatusName", userInfo.StatusId);
+            ViewBag.UserId = new SelectList(db.UserCredentials, "UserId", "Password", userInfo.UserId);
+            return View(userInfo);
+        }
+
+        // GET: UserInfo/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            UserInfo userInfo = db.UserInfoes.Find(id);
+            if (userInfo == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userInfo);
+        }
+
+        // POST: UserInfo/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            UserInfo userInfo = db.UserInfoes.Find(id);
+            db.UserInfoes.Remove(userInfo);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
