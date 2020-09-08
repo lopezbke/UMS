ecc8652829691866403275d05ae9aaa7237d4914
Kevin Lopez <kevin.lopez@email.com>
2020-09-08 01:30:40 -0400

    Changed Default Route

--- UMSV3/App_Start/RouteConfig.cs
+++ UMSV3/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace UMSV3
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "userinfo", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
