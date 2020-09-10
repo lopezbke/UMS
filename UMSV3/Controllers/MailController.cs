22c7ea6be6ed34f1a2bd6a7ce21615774623da96
klopez26 <klopez26@DXCUSAAI421607>
2020-09-10 11:19:34 -0400

    Mail Controller Added.

---
+++ UMSV3/Controllers/MailController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UMSV3.Controllers
+{
+    public class MailController : Controller
+    {
+        // GET: Mail
+        public ActionResult Index()
+        {
+            var a = new Microsoft.Office.Interop.Outlook.Application(); 
+            var b = new Microsoft.Office.Interop.Outlook.MailItem();
+            
+            return View();
+        }
+    }
+}
