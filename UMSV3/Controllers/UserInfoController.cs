074404694c863836fd894c7e28eee435b58a5084
Kevin Lopez <kevin.lopez@email.com>
2020-09-27 02:59:32 -0400

    Added Excel Export. Made application prettier.

--- UMSV3/Controllers/UserInfoController.cs
+++ UMSV3/Controllers/UserInfoController.cs
@@ -13,6 +13,7 @@ using System.Configuration;
 using Microsoft.Ajax.Utilities;
 using System.IO;
 using System.Drawing;
+using System.Runtime.InteropServices;
 
 namespace UMSV3.Controllers
 {
@@ -59,8 +60,10 @@ namespace UMSV3.Controllers
             return "";
         }
         // GET: UserInfo
-        public ActionResult Index()
+        public ActionResult Index(string a,string color)
         {
+            ViewBag.ExcelExport = a;
+            ViewBag.Color = color;
             var userInfoes = db.UserInfoes.Include(u => u.Role).Include(u => u.Status).Include(u => u.UserCredential);
             
             return View(userInfoes.ToList());
@@ -74,13 +77,98 @@ namespace UMSV3.Controllers
                 || u.PhoneNumber == obj );
             return View(userInfoes);
         }
-        private static Image BinaryToImage(byte[] binaryData)
+        [HttpPost]
+        public ActionResult ExportToExcel(string fileName) 
+        {
+            if (fileName == "") { fileName = "UserList"; }
+            Microsoft.Office.Interop.Excel.Workbook ExcelWorkBook;
+            Microsoft.Office.Interop.Excel.Worksheet ExcelWorkSheet;
+            object misValue = System.Reflection.Missing.Value;
+            Microsoft.Office.Interop.Excel.Application ExcelExport = new Microsoft.Office.Interop.Excel.Application();
+            
+            ExcelWorkBook = ExcelExport.Workbooks.Add(misValue);
+            ExcelWorkSheet = ExcelWorkBook.Worksheets.get_Item(1);
+
+            ExcelWorkSheet.Cells.NumberFormat = "@";
+            var rowColor = System.Drawing.ColorTranslator.ToOle(System.Drawing.Color.Yellow);
+
+            ExcelWorkSheet.get_Range("A1", "K1").Interior.Color = rowColor;
+            ExcelWorkSheet.get_Range("A1", "K1").Font.Bold = true;
+            
+
+            ExcelWorkSheet.Cells[1, 1] = "UserName";
+
+            ExcelWorkSheet.Cells[1, 2] = "FirstName";
+
+            ExcelWorkSheet.Cells[1, 3] = "LastName";
+
+            ExcelWorkSheet.Cells[1, 4] = "Email";
+
+            ExcelWorkSheet.Cells[1, 5] = "Address";
+
+            ExcelWorkSheet.Cells[1, 6] = "City";
+
+            ExcelWorkSheet.Cells[1, 7] = "Country";
+
+            ExcelWorkSheet.Cells[1, 8] = "ZipCode";
+
+            ExcelWorkSheet.Cells[1, 9] = "PhoneNumber";
+
+            ExcelWorkSheet.Cells[1, 10] = "Role";
+
+            ExcelWorkSheet.Cells[1, 11] = "Status";
+
+            var userInfoes = db.UserInfoes.Include(u => u.Role).Include(u => u.Status).Include(u => u.UserCredential);
+            userInfoes.ToList();
+            int row = 2;
+            foreach(var item in userInfoes) 
+            {
+
+                ExcelWorkSheet.Cells[row, 1] = item.UserName;
+                ExcelWorkSheet.Cells[row, 2] = item.FirstName;
+                ExcelWorkSheet.Cells[row, 3] = item.LastName;
+                ExcelWorkSheet.Cells[row, 4] = item.Email;
+                ExcelWorkSheet.Cells[row, 5] = item.C_Address;
+                ExcelWorkSheet.Cells[row, 6] = item.City;
+                ExcelWorkSheet.Cells[row, 7] = item.Country;
+                ExcelWorkSheet.Cells[row, 8] = item.ZipCode;
+                ExcelWorkSheet.Cells[row, 9] = item.PhoneNumber;
+                ExcelWorkSheet.Cells[row, 10] = item.Role.RoleName;
+                ExcelWorkSheet.Cells[row, 11] = item.Status.StatusName;
+                row++;
+            }
+            ExcelWorkSheet.Columns.AutoFit();
+            try
+            {
+                ExcelWorkBook.SaveAs($"{fileName}.xls", Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+                ExcelWorkBook.SendMail("klopez184@outlook.com","User List From the User Management System");
+               
+                System.Diagnostics.Process.Start($"E:/Documents/{fileName}.xls");
+                /*ExcelWorkBook.WebPagePreview();*/
+                ExcelWorkBook.Close(true, misValue, misValue);
+                ExcelExport.Quit();
+                
+
+                Marshal.ReleaseComObject(ExcelWorkSheet);
+                Marshal.ReleaseComObject(ExcelWorkBook);
+                Marshal.ReleaseComObject(ExcelExport);
+
+                return RedirectToAction("Index", new { a = "Excel Document was created sucessfully and it should be available in your Documents folder.", color = "green" });
+            }
+            catch 
+            {
+                 System.Diagnostics.Debug.WriteLine("Excel Document was not created.");
+                 return RedirectToAction("Index", new { a = "Excel Document was not created.", color = "red" });
+            }
+            
+        }
+        /*private static Image BinaryToImage(byte[] binaryData)
         {
             MemoryStream ms = new MemoryStream(binaryData);
             Image img = Image.FromStream(ms);
             return img;
         }
-        
+        */
         // GET: UserInfo/Details/5
         public ActionResult Details(int? id)
         {
